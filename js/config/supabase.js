48f7669ccb118c6b71c192f46cd5e485ccb1b762
PratamaAngga <anggashillnoir@gmail.com>
2026-05-22 09:36:39 +0700

    index

--- js/config/supabase.js
+++ js/config/supabase.js
@@ -3,7 +3,5 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 const SUPABASE_URL = 'https://ihwpxhqflghiblbfjonx.supabase.co'
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlod3B4aHFmbGdoaWJsYmZqb254Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcyNzI3MTYsImV4cCI6MjA5Mjg0ODcxNn0.bk_CewautLlPWewjZCXQMKNY8zPF1wkPVZu-VNxOzpc'
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
-const { createClient } = supabase;
-const sb = createClient(SUPABASE_URL, SUPABASE_ANON);
- 
+// Cukup satu export ini saja yang dipakai di semua file
+export const sb = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
